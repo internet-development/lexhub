361e169fbba50473e723cebb0401e00ee41eb73b
Caidan Williams <caidan@internet.dev>
2025-11-19 16:21:42 -0800

    feat: add trigger to update timestamp on the lexicons.updated_at column

--- drizzle/0000_init.sql
+++ drizzle/0000_init.sql
@@ -9,4 +9,18 @@ CREATE TABLE "lexicons" (
 --> statement-breakpoint
 CREATE INDEX "lexicons_id_idx" ON "lexicons" USING btree ("id");--> statement-breakpoint
 CREATE INDEX "lexicons_created_at_idx" ON "lexicons" USING btree ("created_at");--> statement-breakpoint
-CREATE INDEX "lexicons_data_gin_idx" ON "lexicons" USING gin ("data");
+CREATE INDEX "lexicons_data_gin_idx" ON "lexicons" USING gin ("data");--> statement-breakpoint
+
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_lexicons_updated_at
+	BEFORE UPDATE ON "lexicons"
+	FOR EACH ROW
+	EXECUTE FUNCTION update_updated_at_column();
